72467abdf68ff22d7be975039e2cdbd67ffb9c7e
William A <76547127+TANK1-41@users.noreply.github.com>
2022-07-30 02:57:03 -0400

    static building with cmake

--- Agate/src/Agate.h
+++ Agate/src/Agate.h
@@ -2,13 +2,12 @@
 
 #include "Agate/EntryPoint.h"
 #include "Agate/Logger.h"
-#include "Agate/Math/Math.h"
 
 #include "Agate/Events/ApplicationEvents.h"
 #include "Agate/Events/Event.h"
 #include "Agate/Events/MouseEvent.h"
 
-#include "Agate/Layer.h";
+#include "Agate/Layer.h"
 
 //------------main-------------
 #include "Agate/Main.h"
--- Agate/src/Agate/Core.h
+++ Agate/src/Agate/Core.h
@@ -1,11 +1,13 @@
 #pragma once
 
-
+#ifdef Agate_STATIC
+#define API
+#else
 #ifdef Agate_DLL
 #define API __declspec(dllexport)
 #else
 #define API __declspec(dllimport)
 #endif// Core
-
+#endif
 
 #define BindFn(x) std::bind(&x, this, std::placeholders::_1)
