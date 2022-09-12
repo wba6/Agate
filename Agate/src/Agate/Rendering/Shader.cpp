29a31062bd10dba036c0b85f65514a7a45182f4c
William A <76547127+TANK1-41@users.noreply.github.com>
2022-09-12 14:19:47 -0400

    started shader class and update names

---
+++ Agate/src/Agate/Rendering/Shader.cpp
@@ -0,0 +1,9 @@
+//
+// Created by TANK1_41 on 9/12/2022.
+//
+
+#include "Shader.h"
+
+namespace Agate
+{
+}// namespace Agate
---
+++ Agate/src/Agate/Rendering/Shader.h
@@ -0,0 +1,16 @@
+//
+// Created by TANK1_41 on 9/12/2022.
+//
+
+#ifndef AGATE_SHADER_H
+#define AGATE_SHADER_H
+
+namespace Agate
+{
+
+    class Shader {
+    };
+
+}// namespace Agate
+
+#endif//AGATE_SHADER_H
