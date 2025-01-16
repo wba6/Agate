079f4cc6cd705e4dcbfc366fff5b123f8ce47b76
wba6 <bradenaey1@gmail.com>
2025-01-15 19:18:32 -0500

    Update flags for assimp

--- Agate/src/Agate/Rendering/ModelLoader.cpp
+++ Agate/src/Agate/Rendering/ModelLoader.cpp
@@ -114,8 +114,10 @@ void Agate::ModelLoader::loadModel(const std::string &path) {
     // read file via ASSIMP
     Assimp::Importer importer;
     const aiScene *scene = importer.ReadFile(path,
-                                             aiProcess_Triangulate | aiProcess_GenSmoothNormals |
-                                             aiProcess_CalcTangentSpace);//@TODO some models need | aiProcess_FlipUVs
+                                             aiProcess_CalcTangentSpace         |
+                                                     aiProcess_Triangulate             |
+                                                     aiProcess_JoinIdenticalVertices   |
+                                                     aiProcess_SortByPType);//@TODO some models need | aiProcess_FlipUVs
     // check for errors
     if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
     {
