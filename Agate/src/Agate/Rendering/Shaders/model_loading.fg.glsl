cdc994be7e8597c85e8017ba64565911406f2623
wba6 <bradenaey1@gmail.com>
2025-02-02 19:42:10 -0500

    Add diffusion

--- Agate/src/Agate/Rendering/Shaders/model_loading.fg.glsl
+++ Agate/src/Agate/Rendering/Shaders/model_loading.fg.glsl
@@ -2,12 +2,23 @@
 out vec4 FragColor;
 
 in vec2 TexCoords;
+in vec3 Normal;
 
 uniform sampler2D texture_diffuse1;
+//uniform vec4 lightPosition;
+
 
 void main()
 {
-    vec4 ambient = vec4(0.2, 0.2, 0.2, 1.0);
+    vec3 lightPosition = vec3(0.0, 0.0, 0.0);
+    //Calculate the light direction
+    vec3 lightDirection = normalize(lightPosition - vec3(TexCoords, 0.0));
+
+    // Calculate the normal
+    float diff = 1 - max(dot(lightDirection, normalize(Normal)), 0.0);
+
+    // Calculate ambient lighting
+    float ambient = 0.2;
     vec4 textureColor = texture(texture_diffuse1, TexCoords);
-    FragColor = ambient * textureColor;
+    FragColor = (diff + ambient) * textureColor;
 }
