3856d623d9bd66446582c67e73c065798e2076ac
jthorp07 <jackthorp07@gmail.com>
2026-01-23 19:43:44 -0500

    Add doc comments

--- Agate/src/Agate/Core/LayerStack.h
+++ Agate/src/Agate/Core/LayerStack.h
@@ -6,6 +6,13 @@
 
 
 namespace Agate {
+
+    /**
+     * @brief Holds an ordering of layers accessible only by the
+     *        core loop and event system
+     * 
+     * @note See Agate::EntryPoint
+     */
     class LayerStack {
         friend class EntryPoint;
 
@@ -15,19 +22,31 @@ namespace Agate {
         ~LayerStack();
 
         /**
-         * Adds layer to the end of the layer stack but before overlay layers
-         * @param layer
+         * @brief Adds a layer to the end of the layer stack but before overlay layers
+         * 
+         * @param layer Layer to add
          */
         virtual void AddLayer(Layer *layer);
 
+        /**
+         * @brief Removes a layer from the stack
+         * 
+         * @param layer Layer to remove
+         */
         virtual void RemoveLayer(Layer *layer);
 
         /**
-         * adds an overlay layer, overlay layers are the last to be rendered
-         * @param overlay
+         * @brief Adds an overlay layer, overlay layers are the last to be rendered
+         * 
+         * @param overlay Layer to add
          */
         virtual void AddOverlay(Layer *overlay);
 
+        /**
+         * @brief Removes an overlay layer from the stack
+         * 
+         * @param overlay Layer to remove
+         */
         virtual void RemoveOverlay(Layer *overlay);
 
     private:
