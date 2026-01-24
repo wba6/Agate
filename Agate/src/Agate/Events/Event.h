f5bb7e1fcb3f8ac05d44d6a0f726edf6254b4033
jthorp07 <jackthorp07@gmail.com>
2026-01-24 13:33:08 -0500

    Add doc comments

--- Agate/src/Agate/Events/Event.h
+++ Agate/src/Agate/Events/Event.h
@@ -10,9 +10,11 @@
 #include "Agate/Core/Core.h"
 #include "Agate/Core/Logger.h"
 
-
 namespace Agate {
 
+    /**
+     * @brief Built-in event types for the engine
+     */
     enum class API EventTypes {
         MouseButtonPressed,
         MouseButtonReleased,
@@ -24,6 +26,9 @@ namespace Agate {
 
     };
 
+    /**
+     * @brief Base virtual class for engine events. 
+     */
     class API Event {
         friend class EventNotifier;
 
@@ -31,26 +36,56 @@ namespace Agate {
         Event()
                 : EventFinised(false) {}
 
+        /**
+         * @brief Type of this event
+         */
         virtual EventTypes GetEventType() = 0;
 
+        /**
+         * @brief Prints the name of this event
+         */
         virtual void PrintEventName() = 0;
 
+        /**
+         * @brief Predicate for whether the event has
+         *        been fully handled
+         */
         virtual bool Handled() = 0;
 
     protected:
         bool EventFinised;
     };
 
+    /**
+     * @brief Wrapper for event handlers. Consumes a generic
+     *        `Event` and narrows it to a specialization of
+     *        `Event` if it can handle the event
+     */
     class API EventNotifier {
         template<typename T>
         using EventFn = std::function<bool(T &)>;
 
     public:
+
+        /**
+         * @brief Constructor; Creates a notifier for an event
+         * 
+         * @param e Event to handle
+         */
         explicit EventNotifier(Event &e)
                 : m_Event(e) {};
 
+        /**
+         * @brief Template that conditionally handles the underlying
+         *        event if its event type matches that of the template
+         *        argument
+         * 
+         * @param ev Handler for the event, given it is of the
+         *           appropriate type
+         * 
+         * @return True if the event has been handled by this invocation
+         */
         template<typename T>
-
         bool NotifyEvent(EventFn<T> ev) {
             if (m_Event.GetEventType() == T::s_GetEventType()) {
                 m_Event.EventFinised = ev(*(T *) &m_Event);
@@ -62,7 +97,6 @@ namespace Agate {
     private:
         Event &m_Event;
     };
-
 
 }// namespace Agate
 
