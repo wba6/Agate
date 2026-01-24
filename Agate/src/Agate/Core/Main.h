926b6d9704ba1d172842db7053cd52c1caf3c6bb
jthorp07 <jackthorp07@gmail.com>
2026-01-23 21:18:35 -0500

    Add file header comment

--- Agate/src/Agate/Core/Main.h
+++ Agate/src/Agate/Core/Main.h
@@ -1,3 +1,9 @@
+/**
+ * @brief Include file for running an Agate executable. Defines main
+ *        and utilizes `Agate::CreateEntryPoint` (which must be defined
+ *        by consuming applications) to run Agate
+ */
+
 #pragma once
 
 //-----------------------------------------Main Entry -----------------------------------
