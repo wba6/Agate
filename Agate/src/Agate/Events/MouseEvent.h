f04834befa9ecc782eb2a67628a6821b7bb46555
jthorp07 <jackthorp07@gmail.com>
2026-01-26 15:52:27 -0500

    Add doc comments

--- Agate/src/Agate/Events/MouseEvent.h
+++ Agate/src/Agate/Events/MouseEvent.h
@@ -8,33 +8,66 @@
 #include "Event.h"
 
 namespace Agate {
+
+    /**
+     * @brief Emitted when the mouse cursor moves in the application
+     *        window
+     */
     class MouseMoved : public Event {
     public:
+
+        /**
+         * @brief Constructor - Takes the new cursor coordinates
+         * 
+         * @param x New X coordinate of the cursor
+         * @param y New Y coordinate of the cursor
+         */
         MouseMoved(int x, int y)
                 : m_newXPos(x), m_newYPos(y) {}
 
-
+        /**
+         * @brief Type of this event
+         */
         EventTypes GetEventType() override {
             return EventTypes::MouseMoved;
         }
 
-
+        /**
+         * @brief Prints the name of this event
+         */
         void PrintEventName() override {
             PRINTMSG("MouseMoved to: {}, {}", m_newXPos, m_newYPos);
         }
 
+        /**
+         * @brief New X coordinate of the cursor
+         * 
+         * @return X coordinate of the cursor
+         */
         int GetXPos() {
             return m_newXPos;
         }
 
+        /**
+         * @brief New Y coordinate of the cursor
+         * 
+         * @return Y coordinate of the cursor
+         */
         int GetYPos() {
             return m_newYPos;
         }
 
+        /**
+         * @brief Predicate for whether the event has
+         *        been fully handled
+         */
         bool Handled() override {
             return this->EventFinised;
         }
 
+        /**
+         * @brief Type of this event
+         */
         static EventTypes s_GetEventType() {
             return EventTypes::MouseMoved;
         }
@@ -43,8 +76,16 @@ namespace Agate {
         int m_newXPos, m_newYPos;
     };
 
+    /**
+     * @brief Parent for all mouse button events
+     */
     class MouseButton : public Event {
     protected:
+
+        /**
+         * @brief Constructor - Takes an integer representation
+         *        of the emitting mouse button
+         */
         MouseButton(int button)
                 : m_button(button) {}
 
@@ -53,22 +94,40 @@ namespace Agate {
 
     class API MouseButtonPressed : public MouseButton {
     public:
+
+        /**
+         * @brief Constructor - Takes an integer representation
+         *        of the emitting mouse button
+         */
         MouseButtonPressed(int button)
                 : MouseButton(button) {}
 
+        /**
+         * @brief Type of this event
+         */
         EventTypes GetEventType() override {
             return EventTypes::MouseButtonPressed;
         }
 
+        /**
+         * @brief Prints the name of this event
+         */
         void PrintEventName() override {
             std::string eventString = "MousePressed button: " + std::to_string(m_button);
             PRINTMSG("MousePressed button: {}", m_button);
         }
 
+        /**
+         * @brief Predicate for whether the event has
+         *        been fully handled
+         */
         bool Handled() override {
             return this->EventFinised;
         }
 
+        /**
+         * @brief Type of this event
+         */
         static EventTypes s_GetEventType() {
             return EventTypes::MouseButtonPressed;
         }
@@ -76,21 +135,39 @@ namespace Agate {
 
     class API MouseButtonReleased : public MouseButton {
     public:
+
+        /**
+         * @brief Constructor - Takes an integer representation
+         *        of the emitting mouse button
+         */
         MouseButtonReleased(int button)
                 : MouseButton(button) {}
 
+        /**
+         * @brief Type of this event
+         */
         EventTypes GetEventType() override {
             return EventTypes::MouseButtonReleased;
         }
 
+        /**
+         * @brief Prints the name of this event
+         */
         void PrintEventName() override {
             PRINTMSG("MouseReleased button: {}", m_button);
         }
 
+        /**
+         * @brief Predicate for whether the event has
+         *        been fully handled
+         */
         bool Handled() override {
             return this->EventFinised;
         }
 
+        /**
+         * @brief Type of this event
+         */
         static EventTypes s_GetEventType() {
             return EventTypes::MouseButtonReleased;
         }
