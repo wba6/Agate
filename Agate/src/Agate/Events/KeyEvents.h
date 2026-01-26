0f5bc862d3011bc50ae150f759594b7d34c1a0b2
jthorp07 <jackthorp07@gmail.com>
2026-01-26 15:41:12 -0500

    Add doc comments

--- Agate/src/Agate/Events/KeyEvents.h
+++ Agate/src/Agate/Events/KeyEvents.h
@@ -10,27 +10,56 @@
 #define AGATE_KEYEVENTS_H
 
 namespace Agate {
+
+    /**
+     * @brief Emitted when any key is pressed
+     */
     class KeyPressedEvent : public Event {
     public:
+
+        /**
+         * @brief Constructor - takes a keycode to identify the
+         *        pressed key
+         * 
+         * @param keyCode Keycode representing the pressed key
+         */
         KeyPressedEvent(int keyCode)
                 : m_keycode(keyCode) {};
 
+        /**
+         * @brief Type of this event
+         */
         EventTypes GetEventType() override {
             return EventTypes::KeyPressed;
         }
 
+        /**
+         * @brief Prints the name of this event
+         */
         void PrintEventName() override {
             PRINTMSG("KeyPressedEvent: {}", m_keycode);
         }
 
+        /**
+         * @brief Predicate for whether the event has
+         *        been fully handled
+         */
         bool Handled() override {
             return this->EventFinised;
         }
 
+        /**
+         * @brief Keycode for the key that has been pressed
+         * 
+         * @return Keycode representing a keyboard key
+         */
         unsigned int GetKeyCode() {
             return m_keycode;
         }
 
+        /**
+         * @brief Type of this event
+         */
         static EventTypes s_GetEventType() {
             return EventTypes::KeyPressed;
         }
@@ -39,27 +68,55 @@ namespace Agate {
         unsigned int m_keycode;
     };
 
+    /**
+     * @brief Emitted when any key is released
+     */
     class KeyReleasedEvent : public Event {
     public:
+
+        /**
+         * @brief Constructor - takes a keycode to identify the
+         *        released key
+         * 
+         * @param keyCode Keycode representing the released key
+         */
         KeyReleasedEvent(int keyCode)
                 : m_keycode(keyCode) {};
 
+        /**
+         * @brief Type of this event
+         */
         EventTypes GetEventType() override {
             return EventTypes::KeyReleased;
         }
 
+        /**
+         * @brief Prints the name of this event
+         */
         void PrintEventName() override {
             PRINTMSG("KeyReleasedEvent: {}", m_keycode);
         }
 
+        /**
+         * @brief Predicate for whether the event has
+         *        been fully handled
+         */
         bool Handled() override {
             return this->EventFinised;
         }
 
+        /**
+         * @brief Keycode for the key that has been released
+         * 
+         * @return Keycode representing a keyboard key
+         */
         unsigned int GetKeyCode() {
             return m_keycode;
         }
 
+        /**
+         * @brief Type of this event
+         */
         static EventTypes s_GetEventType() {
             return EventTypes::KeyReleased;
         }
