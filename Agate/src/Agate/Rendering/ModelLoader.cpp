137efa28bd790b96117d5a6391aedbd5c490c0ef
= <=>
2026-02-16 14:35:24 -0500

    Use status result from load task to log warning on load failure

--- Agate/src/Agate/Rendering/ModelLoader.cpp
+++ Agate/src/Agate/Rendering/ModelLoader.cpp
@@ -18,8 +18,11 @@ void Agate::ModelLoader::Draw(Agate::Shader &shader) {
         auto status = m_futureScene.wait_for(std::chrono::seconds(0));
         if (status == std::future_status::ready) {
             // Once this is done the future scene will no longer be valid
-            m_futureScene.get();
-            m_meshes = parseModel();
+            if (m_futureScene.get()) {
+                m_meshes = parseModel();
+            } else {
+                PRINTWARN("ModelLoader: Model load failed");
+            }
         }
     }
 
