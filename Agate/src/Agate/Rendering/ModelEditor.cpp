6c54ca99367046ffc438636a23287550a149c580
jthorp07 <jackthorp07@gmail.com>
2026-02-26 18:38:25 -0500

    Fix default rotation quaternion to indicate no rotation

--- Agate/src/Agate/Rendering/ModelEditor.cpp
+++ Agate/src/Agate/Rendering/ModelEditor.cpp
@@ -6,7 +6,7 @@ namespace Agate {
 Transform::Transform()
         : position(glm::vec3(0, 0, 0)),
           scale(glm::vec3(1, 1, 1)),
-          rotation(glm::quat(0, 1, 0, 0))  {}
+          rotation(glm::quat(1, 0, 0, 0))  {}
 
 ModelEditor::ModelEditor(std::string path, std::vector<Mesh> meshes)
         : m_path(path), m_meshes(meshes) {}
