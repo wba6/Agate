630854ec63de916904211829c341c6059c53813d
= <=>
2026-03-10 13:31:06 -0400

    Move lock to make TaskHandle::Get entirely atomic

--- Agate/src/Agate/Async/TaskPool.h
+++ Agate/src/Agate/Async/TaskPool.h
@@ -122,12 +122,12 @@ template<typename ResultType>
 ResultType TaskHandle<ResultType>::Get() {
 
     // Critical error: Invalid access attempt
+    std::scoped_lock lock(sharedState->mutex);
     if (!sharedState->result.has_value() || Status() != TaskStatus::Done) {
         PRINTCRIT("Attempted to call `TaskHandle::Get` with an invalid result");
         throw std::runtime_error("Invalid std::optional access attempt");
     }
 
-    std::scoped_lock lock(sharedState->mutex);
     ResultType result = std::move(*(sharedState->result));
     sharedState->result.reset();
     return result;
